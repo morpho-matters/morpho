9d8d8fd76d2a20ad9942368b7554577f5dedb77b
morpho-matters <37314443+morpho-matters@users.noreply.github.com>
2018-06-01 19:06:15 -0500

    Combined launch_morpho.py and player.py

--- launch_morpho.py
+++ launch_morpho.py
@@ -1,3 +1,26 @@
-import morpho.gui
+from tkinter import messagebox as dialog
+import morpho.gui as gui
+import sys
 
-morpho.gui.startGUI()
+def runMRM(filename=gui.dotslash+"lastplay.mrm"):
+    try:
+        win = gui.RootWindow(gui.defaultSettings)
+        win.root.withdraw()
+        try:
+            win.load(filename)
+        except:
+            dialog.showerror(
+                "Load Error",
+                "Morpho can't access lastplay.mrm in its own directory. This is necessary in order to play the animation."
+                )
+            return
+        win._run()
+    except:
+        gui.showStandardError()
+        raise
+
+# If an MRM filename is passed to the script, run it!
+if len(sys.argv) > 1:
+    runMRM(sys.argv[-1])
+else:  # Normal startup
+    gui.startGUI()
