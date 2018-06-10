b981db1847e47b5a3f73c83aa19c976b443bcf84
morpho-matters <37314443+morpho-matters@users.noreply.github.com>
2018-06-10 00:57:06 -0500

    Added dash where forgotten on "-O3"

--- morpho/giffer.py
+++ morpho/giffer.py
@@ -42,10 +42,10 @@ def optimizegif(filename):
     if not os.path.isfile(filename):
         raise FileNotFoundError
     if platform.system() == "Windows":
-        cmd = [".\\gifsicle.exe", "-b", "O3", "--careful", filename]
+        cmd = [".\\gifsicle.exe", "-b", "-O3", "--careful", filename]
         # cmd = '.\\gifsicle.exe -b -O3 --careful "' + filename + '"'
         sp.call(cmd, creationflags=CREATE_NO_WINDOW)
     else:
-        # cmd = os.curdir + os.sep + 'gifsicle -b O3 --careful "' + filename + '"'
-        cmd = [dotslash+"gifsicle", "-b", "O3", "--careful", filename]
+        # cmd = os.curdir + os.sep + 'gifsicle -b -O3 --careful "' + filename + '"'
+        cmd = [dotslash+"gifsicle", "-b", "-O3", "--careful", filename]
         sp.call(cmd)
