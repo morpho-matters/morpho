5f948184afd260ff215e2a477d8bbb6a402712b3
morpho-matters <37314443+morpho-matters@users.noreply.github.com>
2018-06-15 14:37:59 -0500

    resources/ directory now used

--- morpho/giffer.py
+++ morpho/giffer.py
@@ -42,10 +42,8 @@ def optimizegif(filename):
     if not os.path.isfile(filename):
         raise FileNotFoundError
     if platform.system() == "Windows":
-        cmd = [".\\gifsicle.exe", "-b", "-O3", "--careful", filename]
-        # cmd = '.\\gifsicle.exe -b -O3 --careful "' + filename + '"'
+        cmd = [".\\resources\\gifsicle.exe", "-b", "-O3", "--careful", filename]
         sp.call(cmd, creationflags=CREATE_NO_WINDOW)
     else:
-        # cmd = os.curdir + os.sep + 'gifsicle -b -O3 --careful "' + filename + '"'
-        cmd = [pwd+"gifsicle", "-b", "-O3", "--careful", filename]
+        cmd = [pwd+"resources"+os.sep+"gifsicle", "-b", "-O3", "--careful", filename]
         sp.call(cmd)
